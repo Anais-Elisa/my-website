bd92cd6878d071eb7874e763b7ddb54f55422a2a
Anais-Elisa <anaisk.elisabeth@gmail.com>
2023-05-31 15:35:09 +0200

    restart

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Header from './conponents/Header';
-import About from './conponents/About';
-import Projects from './conponents/Projects';
-import Navbar from  './conponents/Navbar'; 
+import logo from './logo.svg';
 import './App.css';
-
 
 function App() {
   return (
-    <main>
-      <Navbar />
-    </main>
- 
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
