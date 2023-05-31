7e8403ac337b56332e56f903fa7c2e3ce87dcafb
Anais-Elisa <anaisk.elisabeth@gmail.com>
2023-05-31 11:29:37 +0200

    test ananas

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section>
+      <h2>À propos de moi</h2>
+      <p>Je suis un développeur passionné avec une expérience dans les technologies web.</p>
+    </section>
+  );
+}
+
+export default About;
